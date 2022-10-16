10e3052135c28917ae7606a1c2e704bb4ea9abba
Linus Mellberg <linus.mell04@gmail.com>
2022-10-16 16:35:06 +0200

    draw func and close work

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ class Game():
         self.window = pygame.display.set_mode((WIDTH,HEIGHT))
         pygame.display.set_caption(CAPTION)
 
+    def draw(self):
+        self.window.fill((20,12,14))
+        pygame.display.update()
+
     def handle_quit(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
+                pygame.quit()
+                sys.exit()
+
 
     def handle_input(self):
         keys_pressed = pygame.key.get_pressed()
@@ -23,13 +29,11 @@ class Game():
             pygame.quit()
             sys.exit()
 
-        self.handle_quit()
-
     def run(self):
         while True:
-            self.window.fill((20,12,14))
             self.handle_input()
-            pygame.display.update()
+            self.handle_quit()
+            self.draw()
 
 if __name__ == "__main__":
     game = Game()
